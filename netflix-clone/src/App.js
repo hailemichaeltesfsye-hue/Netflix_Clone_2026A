ac81af26d4b51fe5532951bfa543bf53837cc8d7
hailemichael tesfaye <your.hailemichaeltesfsye@gmail.com>
2026-07-15 05:39:11 +0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-     <h1>Netflix Clone 2026A</h1>
+     <Home/>
     </div>
   );
 }
